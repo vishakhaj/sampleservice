9d7ca6543f796afbed1a1effeab2ea9ba644a068
vishakha <vishakhaj.18@gmail.com>
2017-05-13 17:28:20 +0200

    Fix consul set-up for Rancher

--- sampleservice/src/main/java/com/sampleservice/SampleserviceApplication.java
+++ sampleservice/src/main/java/com/sampleservice/SampleserviceApplication.java
@@ -2,10 +2,12 @@ package com.sampleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableDiscoveryClient
 public class SampleserviceApplication {
 
 	public static void main(String[] args) {
